c3f2e50865f3b72e5474641be75fe74a1a694276
LuciaMarcu <lucia_sanzianu@yahoo.com>
2021-10-22 10:36:39 +0300

    Count the occurencies

--- k_scores.rb
+++ k_scores.rb
@@ -1,4 +1,9 @@
-def scores(n, k, numbers)
-    
-
-end
+def scores(numbers)
+    occ_hash = Hash.new(0)
+    numbers.each do |key|
+      occ_hash[key] +=1
+    end
+     p occ_hash 
+  end
+  
+  scores( [6, 5, 2, 6, 6, 2, 1, 7, 3, 3, 3])
