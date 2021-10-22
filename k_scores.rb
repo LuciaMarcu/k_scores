50f1af5e05ab93ab041d2f20f51aa1a7970b7dc8
LuciaMarcu <lucia_sanzianu@yahoo.com>
2021-10-22 11:04:09 +0300

    Sorted hash

--- k_scores.rb
+++ k_scores.rb
@@ -1,9 +1,10 @@
-def scores(numbers)
+def scores(numbers, k)
     occ_hash = Hash.new(0)
     numbers.each do |key|
       occ_hash[key] +=1
     end
-     p occ_hash 
-  end
+
+    sorted = occ_hash.sort_by {|key, v| -v}
+end
   
-  scores( [6, 5, 2, 6, 6, 2, 1, 7, 3, 3, 3])
+  scores( [6, 5, 2, 6, 6, 2, 1, 7, 3, 3, 3], k)
