df7c12b14f30243df4d6c77f9540b50594466fcc
LuciaMarcu <lucia_sanzianu@yahoo.com>
2021-10-22 12:23:44 +0300

    Version v1

--- k_scores.rb
+++ k_scores.rb
@@ -1,10 +1,20 @@
-def scores(numbers, k)
-    occ_hash = Hash.new(0)
+def scores(numbers, k, n)
+    
+    occurences_hash = Hash.new(0)
     numbers.each do |key|
-      occ_hash[key] +=1
+      occurences_hash[key] +=1
     end
 
-    sorted = occ_hash.sort_by {|key, v| -v}
+    sorted_arr = occurences_hash.sort_by {|key, v| -v}
+    final_arr = []
+    i = 0
+    while i < k && i < sorted_arr.length do 
+       final_arr << sorted_arr[i][0]
+       i += 1
+    end
+
+    final_arr
+
 end
   
-  scores( [6, 5, 2, 6, 6, 2, 1, 7, 3, 3, 3], k)
+  scores( [6, 5, 2, 6, 6, 2, 1, 7, 3, 3, 3], 4, 6)
